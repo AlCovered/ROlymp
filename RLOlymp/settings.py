a93f30f63229a9975faafb7859f6379451e278e3
Shurgentum <k.asyeyev@gmail.cm>
2019-06-20 14:11:21 +0300

    Initial Update

--- RLOlymp/settings.py
+++ RLOlymp/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Main.apps.MainConfig',
+    'Users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
