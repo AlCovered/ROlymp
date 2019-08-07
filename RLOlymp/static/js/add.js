27fa46ff995ca2cd428e37af740ca2d0abbee33e
AlexShurgent <Alexwinner03@gmail.com>
2019-08-07 09:15:03 +0300

    Change directory

--- RLOlymp/static/js/add.js
+++
@@ -1,9 +0,0 @@
-function ChangeName(event){
-    var element = document.getElementById("change-name");
-    element.innerHTML = "Пользователи";
-}
-
-function BackToPast(event){
-    var element = document.getElementById("change-name");
-    element.innerHTML = "Пользователь";
-}
