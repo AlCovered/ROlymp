994dc48781bc97b0eaeabb0b7be1c4de37535d5e
AlexShurgent <Alexwinner03@gmail.com>
2019-08-07 09:16:11 +0300

    Add changes

---
+++ static/js/add.js
@@ -0,0 +1,9 @@
+function ChangeName(event){
+    var element = document.getElementById("change-name");
+    element.innerHTML = "Пользователи";
+}
+
+function BackToPast(event){
+    var element = document.getElementById("change-name");
+    element.innerHTML = "Пользователь";
+}
